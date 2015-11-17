e2a0d6eddd190a2709a7303e8c48b6b12f90e5fb
tannert44 <tannert8155@gmail.com>
2015-11-17 07:02:59 -0600

    Browse Template ready for firebase

--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -2,9 +2,9 @@ define(function(require) {
   var $ = require("jquery");
   var b = require("bootstrap");
   var h = require("hbs");
-<<<<<<< HEAD
+
   var profileForm = require("profile-form");
-=======
+
   var browse = require("browse");
->>>>>>> TTT-BrowseTemplate
+
 });
