8cfea5266512bc1d1c08438a5fcf099046b3df8d
Jessawynne Parker <jessawynne@gmail.com>
2015-11-17 12:43:04 -0600

    styled radio buttons

--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -3,4 +3,5 @@ define(function(require) {
   var b = require("bootstrap");
   var h = require("hbs");
   var profileForm = require("profile-form");
+  var formButtons = require("form-button-info");
 });
---
+++ javascripts/form-button-info.js
@@ -0,0 +1,7 @@
+define(function(require) {
+
+//   $('.uiButton').click(function() {
+//     $(this).toggleClass(//active state);
+// });
+
+}); // <-- end of define function
